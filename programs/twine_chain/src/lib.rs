5815823b519d1327631a69da49e13c8aebaf98ab
Sarthak-5harma <sarthak.sharma@cedro.finance>
2024-12-03 21:16:13 +0545

    Deletion from queue after finalization.

--- programs/twine_chain/src/lib.rs
+++ programs/twine_chain/src/lib.rs
@@ -44,16 +44,32 @@ pub mod twine_chain {
     // Commit batch (insert into CommitmentData)
     pub fn commit_batch(ctx: Context<CommitBatch>, commit_info: CommitBatchInfo) -> Result<()> {
         let commitment_data_pda = &mut ctx.accounts.commitment_data_pda;
-
+        let public_input = Vec::new();
         // Insert the commit info into the BTreeMap
-        commitment_data_pda.mapping.push((commit_info.batch_number, commit_info));
+        let batch_to_commit = StoredBatchInfo{
+            batch_number: commit_info.batch_number,
+            batch_hash: commit_info.batch_hash,
+            previous_state_root: commit_info.previous_state_root,
+            state_root: commit_info.state_root,
+            public_input: public_input,
+        };
+        commitment_data_pda.last_committed_batch_number = batch_to_commit.batch_number;
+        commitment_data_pda.mapping.push((batch_to_commit.batch_number, batch_to_commit));
+
         Ok(())
     }
 
     // Finalize batch (remove from CommitmentData)
-    // pub fn finalize_batch(ctx: Context<CommitBatch>, batch_number: u64, proof_bytes: Vec<u8>, public_input: Vec<u8>) -> Result<()> {
+    pub fn finalize_batch(ctx: Context<FinalizeBatch>, batch_number: u64, proof_bytes: Vec<u8>, number_of_deposits: u64) -> Result<()> {
+        let commitment_data_pda = &mut ctx.accounts.commitment_data_pda;
+        let deposit_message_pda = &mut ctx.accounts.deposit_message_pda;
 
-    //     // Get the SP1 Groth16 verification key from the `sp1-solana` crate
+        let public_input = if let Some((_, stored_batch_info)) = commitment_data_pda.mapping.iter().find(|(num, _)| *num == batch_number) {
+            stored_batch_info.public_input.clone()
+        } else {
+            return Err(ErrorCode::BatchNotCommitted)?;
+        };
+    // Get the SP1 Groth16 verification key from the `sp1-solana` crate
     //     let vk = sp1_solana::Groth16_VK_2_0_0_BYTES;
     //     // Verify the Proof
     //     sp1_solana::verify_proof(
@@ -62,8 +78,11 @@ pub mod twine_chain {
     //         &VKEY_HASH,
     //         vk,
     //     ).ok_or(ErrorCode::InvalidProof)?;
-    //     Ok(())
-    // }
+
+        let number_of_deposits_usize = number_of_deposits as usize;
+        deposit_message_pda.deposits.drain(0..number_of_deposits_usize);
+        Ok(())
+    }
 
 }
 
@@ -87,7 +106,7 @@ pub struct InitializeCommitmentDataPDA<'info> {
     #[account(
         init,
         payer = user,
-        space = DISCRIMINATOR + 4 + (MAX_QUEUE_SIZE * CommitBatchInfo::LEN),
+        space = DISCRIMINATOR + 4 + (MAX_QUEUE_SIZE * StoredBatchInfo::LEN),
         seeds = [b"commitment_data_pda"],
         bump,
     )]
@@ -111,6 +130,25 @@ pub struct CommitBatch<'info> {
 }
 
 #[derive(Accounts)]
+pub struct FinalizeBatch<'info> {
+    #[account(
+        mut,
+        seeds = [b"commitment_data_pda"],
+        bump,
+    )]
+    pub commitment_data_pda: Account<'info, CommitmentData>,
+    #[account(
+        mut,
+        seeds = [b"deposit_message_pda"] ,
+        bump,
+     )]
+     pub deposit_message_pda: Account<'info, DepositMessagePDA>,
+     #[account(mut)]
+     pub user: Signer<'info>,
+     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
 pub struct AppendDepositMessage<'info> {
     #[account(
        mut,
@@ -129,7 +167,9 @@ pub struct DepositMessagePDA {
 
 #[account]
 pub struct CommitmentData {
-    pub mapping: Vec<(u64, CommitBatchInfo)>, // Batch number => CommitBatchInfo
+    pub mapping: Vec<(u64, StoredBatchInfo)>, // Batch number => StoredBatchInfo
+    pub last_committed_batch_number: u64,
+    pub last_finalized_batch_number: u64,
 }
 
 // The DepositInfo struct for individual deposit events
@@ -152,10 +192,18 @@ pub struct CommitBatchInfo {
     pub state_root: [u8; 32],
 }
 
-impl CommitBatchInfo {
-    const LEN: usize = 8 + 32 + 32 + 32; // Adjust size for u64 + three 32-byte vectors
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct StoredBatchInfo {
+    pub batch_number: u64,
+    pub batch_hash: [u8; 32],
+    pub previous_state_root: [u8; 32],
+    pub state_root: [u8; 32],
+    pub public_input: Vec<u8>,
 }
 
+impl StoredBatchInfo {
+    const LEN: usize = 8 + 32 + 32 + 32 + 100; // Adjust size for u64 + three 32-byte vectors
+}
 
 // Event for deposits
 #[event]
@@ -170,4 +218,6 @@ pub struct DepositSuccessful {
 pub enum ErrorCode {
     #[msg("Invalid input to the proof")]
     InvalidProof,
+    #[msg("Batch needs to be committed before finalization")]
+    BatchNotCommitted,
 }
